a58c6abd64c634823173e29c76c0dedde1a9d37c
Thunf <458859360@qq.com>
2016-02-16 17:09:47 +0800

    移除实验log

--- cmd/dev.sh
+++ cmd/dev.sh
@@ -3,17 +3,3 @@
 curdir=$(cd `dirname $0`;pwd);
 
 sh $curdir"/log.sh" -gn "启动: 开发模式" -t;
-
-
-# test
-while getopts ":bd" arg #选项后面的冒号表示该选项需要参数
-do
-        case $arg in
-             b)
-                echo "b ==> 背景色"
-                ;;
-             d)
-                echo "d ==> 深色"
-                ;;
-        esac
-done
